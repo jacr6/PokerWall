d97bf535e6862b03e9179bc4c3e45d6cba6290a8
José Cuevas <jose.cuevas.cv@gmail.com>
2022-06-28 18:05:59 -0500

    server

--- server.py
+++ server.py
@@ -3,21 +3,36 @@
 import asyncio
 import websockets
 from websockets import serve
+import json
 
-CLIENTS = set() 
+
+CLIENTS = set()
 
 
 async def echo(websocket):
     async for message in websocket:
         websockets.broadcast(CLIENTS, message)
 
+
 async def handler(websocket):
     CLIENTS.add(websocket)
     await echo(websocket)
-    
 
-async def main():
-    async with serve(handler, "localhost", 5000):
+
+async def main(server, port):
+    async with serve(handler, server, port):
         await asyncio.Future()  # run forever
 
-asyncio.run(main())
+
+def start():
+    config = ""
+    with open('assets/config.json') as config_file:
+        config = json.load(config_file)
+    server = config["server"] or "localhost"
+    port = config["port"] or 5000
+    print(f"Listen on {server}:{port}")
+    asyncio.run(main(server, port))
+
+
+if __name__ == "__main__":
+    start()
